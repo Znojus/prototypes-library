37bc5d500288c78226946656f8f428e6b3ddd852
Znojus <150673971+Znojus@users.noreply.github.com>
2026-02-18 17:56:28 +0200

    Added some JS to display the book object contents
    I've used the innerText method instead of a more
    readable innerHTML option, as I wanted to avoid the
    security risks. Later, the function for user to
    enter his own book will be implemented, which can
    lead to XSS

--- daScript.js
+++ daScript.js
@@ -22,4 +22,41 @@ function addBookToLibrary(author, title, pages, year, isRead) {
 
 addBookToLibrary("Ray Bradbury", "Fahrenheit 451", 227, 2008, true);
 addBookToLibrary("Ar zinai?", "Bredas Pitas", 228, 2001, false)
+
+const cardContainer = document.querySelector(".card-container")
+
+cardContainer.style.backgroundColor = "beige";
+
+myLibrary.forEach(book => {
+    let tempDiv = document.createElement("div");
+    tempDiv.style.backgroundColor = "honeydew";
+
+    let titleDiv = document.createElement("div");
+    let authorDiv = document.createElement("div");
+    let pagesDiv = document.createElement("div");
+    let yearDiv = document.createElement("div");
+    let isReadDiv = document.createElement("div");
+
+    titleDiv.setAttribute("class", "book-title");
+    authorDiv.setAttribute("class", "book-author");
+    pagesDiv.setAttribute("class", "book-pages");
+    yearDiv.setAttribute("class", "book-year");
+    isReadDiv.setAttribute("class", "book-read");
+
+    authorDiv.innerText = book.author;
+    titleDiv.innerText = book.title;
+    pagesDiv.innerText = book.pages;
+    yearDiv.innerText = book.year;
+    isReadDiv.innerText = book.isRead ? "Read" : "Not Read";
+
+    tempDiv.appendChild(titleDiv);
+    tempDiv.appendChild(authorDiv);
+    tempDiv.appendChild(pagesDiv);
+    tempDiv.appendChild(yearDiv);
+    tempDiv.appendChild(isReadDiv);
+
+    cardContainer.appendChild(tempDiv);
+});
+
+
 console.log(myLibrary);
