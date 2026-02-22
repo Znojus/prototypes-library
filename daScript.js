d776be286c3e363d725686e2c061cb726b07a7e6
Znojus <150673971+Znojus@users.noreply.github.com>
2026-02-22 14:13:29 +0200

    Add one more predef book

--- daScript.js
+++ daScript.js
@@ -13,15 +13,13 @@ function Book(author, title, pages, year, isRead) {
     this.id = crypto.randomUUID()
 }
 
-const book = new Book("Plato", "The Republic", 484, 2002, true);
-console.log(book);
-
 function addBookToLibrary(author, title, pages, year, isRead) {
     myLibrary.push(new Book(author, title, pages, year, isRead));
 }
 
 addBookToLibrary("Ray Bradbury", "Fahrenheit 451", 227, 2008, true);
 addBookToLibrary("Ar zinai?", "Bredas Pitas", 228, 2001, false)
+addBookToLibrary("Plato", "The Republic", 484, 2002, true);
 
 const cardContainer = document.querySelector(".card-container")
 
